af63ed5322bf6319e74e79c3db57431b75874cc4
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2024-11-23 00:14:11 +0000

    fixed sidebar css (1)

--- view/profile.php
+++ view/profile.php
@@ -96,6 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/profile.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../assets/css/variables.css">
 </head>
 <body>
     <?php include_once '../components/sidebar.php'; ?>
